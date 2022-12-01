480146e20b6c61a5f7436ad5ae56cbffbc39f527
Pinkman669 <114657343+Pinkman669@users.noreply.github.com>
2022-12-01 19:35:31 +0000

    homepage

---
+++ script.js
@@ -0,0 +1,10 @@
+$(function() { // Function to run the js when DOM is ready
+    $(".toggle").on("click", function(){ // To make the menu bar shown up when click on toggle //
+        if($(".item").hasClass("active")){
+            $(".item").removeClass("active");
+        }
+        else{
+            $(".item").addClass("active");
+        }
+    })
+});
